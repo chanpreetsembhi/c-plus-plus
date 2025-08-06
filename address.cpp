a388ccd277c33ca9aa55f4f0def45bcb6844053b
Chanpreet Sembhi <chanpreetsingh73553@gmail.com>
2025-08-06 05:58:50 +0530

    add address.cpp file

---
+++ address.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    const auto a = new int;
+    const auto b = new int;
+    const auto c = new int;
+
+    *a = 10;
+    *b = 20;
+    *c = *a + *b;
+
+    cout << "Sum of " << *a << " and " << *b << " is " << *c;
+
+    // Free allocated memory
+    delete a;
+    delete b;
+    delete c;
+}
